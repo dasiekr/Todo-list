dff0d88e89f5929a0b32f58117936aa8bfd959e4
dasiekr <dasikr>
2019-10-09 15:26:36 +0200

    Added User and Rol models

--- todo-list/src/main/java/com/todopoject/springBoot/todolist/model/User.java
+++ todo-list/src/main/java/com/todopoject/springBoot/todolist/model/User.java
@@ -1,4 +1,92 @@
 package com.todopoject.springBoot.todolist.model;
 
+import org.springframework.stereotype.Controller;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Entity
+@Table(name = "auth_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "auth_user_id")
+    private int id;
+
+    @Column(name = "user_name")
+    @Size(min = 6, max = 15)
+    private String userName;
+
+    @Column(name = "password")
+    @Size(min = 6, max = 15)
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "auth_user_role", joinColumns = @JoinColumn(name = "auth_user_id"), inverseJoinColumns = @JoinColumn(name = "auth_role_id"))
+    private Set<Role> roles;
+
+    protected User() {super();}
+
+    public User(@Size(min = 6, max = 15) String userName, @Size(min = 6, max = 15) String password, String confirmPassword, Set<Role> roles) {
+        this.userName = userName;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.roles = roles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", confirmPassword='" + confirmPassword + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
 }

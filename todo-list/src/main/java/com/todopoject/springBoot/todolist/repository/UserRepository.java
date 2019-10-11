10fc695f87a96bab2fb2a21a09d139d496782f80
dasiekr <dasikr>
2019-10-11 17:36:31 +0200

    Added UserDetails service

--- todo-list/src/main/java/com/todopoject/springBoot/todolist/repository/UserRepository.java
+++ todo-list/src/main/java/com/todopoject/springBoot/todolist/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.todopoject.springBoot.todolist.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
 }
--- todo-list/src/main/java/com/todopoject/springBoot/todolist/service/CustomUserDetailsService.java
+++ todo-list/src/main/java/com/todopoject/springBoot/todolist/service/CustomUserDetailsService.java
@@ -1,10 +1,27 @@
 package com.todopoject.springBoot.todolist.service;
 
+import com.todopoject.springBoot.todolist.model.User;
+import com.todopoject.springBoot.todolist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository repository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUsername(username);
+        CustomUserDetails userDetails = null;
+        if(user != null) {
+            userDetails = new CustomUserDetails();
+        }else{
+            throw new UsernameNotFoundException("User not exist with name : " + username);
+        }
 
+        return null;
+    }
 }
